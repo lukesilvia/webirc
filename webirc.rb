cf145d3198508461addb743a1e8ccbe90c645f76
lukesilvia <lukesilvia@gmail.com>
2009-05-11 00:04:36 +0900

    Fix: do spec webirc_spec.rb anywhere

--- webirc.rb
+++ webirc.rb
@@ -6,10 +6,11 @@ require 'sinatra' # sudo gem install sinatra -v 0.9.1.1
 require 'yaml'
 require 'models/log'
 
-log = Log.new(YAML.load(File.read('config.yml')))
+log = Log.new(YAML.load(File.read(File.dirname(__FILE__) + '/config.yml')))
 
 get '/' do
   @statuses = log.recent
+
   erb :index
 end
 
